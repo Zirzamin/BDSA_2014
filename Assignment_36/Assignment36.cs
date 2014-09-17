777c04cf016b370ee4b23cf7f110a1dbdf9a72bb
Mehr <mehrdad.khodaverdi@outlook.com>
2014-09-17 16:05:54 +0200

    Mehr First time

---
+++ Assignment_36/Assignment36.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_36
+{
+    internal class Assignment36
+    {
+        private static void Main()
+        {
+            //Evaluating 
+            string eval = "5 1 2 + 4 * + 3 - 12 - 5 ^"; // result 14
+            Console.WriteLine(eval);
+
+            string[] split = eval.Split(' ');
+            List<double> stack = new List<double>();
+
+            double tempResult = 0;
+
+            for (int i = 0; i < split.Length; i++)
+            {
+                double temp;
+                if (double.TryParse(split[i], out temp))
+                {
+                    stack.Add(temp);
+                }
+                else
+                {
+                    try
+                    {
+                        switch (split[i])
+                        {
+                            case "+":
+                                if (stack.Count < 2)
+                                    throw new Exception("Not enough values in stack");
+
+                                tempResult = stack[stack.Count - 2] + stack[stack.Count - 1];
+                                stack.RemoveAt(stack.Count - 2);
+                                stack.RemoveAt(stack.Count - 1);
+                                stack.Add(tempResult);
+                                break;
+                            case "-":
+                                if (stack.Count < 2)
+                                    throw new Exception("Not enough values in stack");
+
+                                tempResult = stack[stack.Count - 2] - stack[stack.Count - 1];
+                                stack.RemoveAt(stack.Count - 2);
+                                stack.RemoveAt(stack.Count - 1);
+                                stack.Add(tempResult);
+                                break;
+                            case "*":
+                                if (stack.Count < 2)
+                                    throw new Exception("Not enough values in stack");
+
+                                tempResult = stack[stack.Count - 2] * stack[stack.Count - 1];
+                                stack.RemoveAt(stack.Count - 2);
+                                stack.RemoveAt(stack.Count - 1);
+                                stack.Add(tempResult);
+                                break;
+                            case "/":
+                                if (stack[stack.Count - 1] == 0x0)
+                                    throw new Exception("cant devide with 0");
+
+                                if (stack.Count < 2)
+                                    throw new Exception("Not enough values in stack");
+
+                                tempResult = stack[stack.Count - 2] / stack[stack.Count - 1];
+                                stack.RemoveAt(stack.Count - 2);
+                                stack.RemoveAt(stack.Count - 1);
+                                stack.Add(tempResult);
+                                break;
+
+                            case "^":
+                                if (stack.Count < 2)
+                                    throw new Exception("Not enough values in stack");
+
+                                tempResult = Math.Pow(stack[stack.Count - 2], stack[stack.Count - 1]);
+                                stack.RemoveAt(stack.Count - 2);
+                                stack.RemoveAt(stack.Count - 1);
+                                stack.Add(tempResult);
+                                break;
+
+                            case "cos":
+                                if (stack.Count < 1)
+                                    throw new Exception("Not enough values in stack");
+
+                                tempResult = Math.Cos(stack[stack.Count - 1]);
+                                stack.RemoveAt(stack.Count - 2);
+                                stack.RemoveAt(stack.Count - 1);
+                                stack.Add(tempResult);
+                                break;
+
+                            case "sqrt":
+                                if (stack.Count < 1)
+                                    throw new Exception("Not enough values in stack");
+
+                                tempResult = Math.Sqrt(stack[stack.Count - 1]);
+                                stack.RemoveAt(stack.Count - 2);
+                                stack.RemoveAt(stack.Count - 1);
+                                stack.Add(tempResult);
+                                break;
+                            default:
+                                throw new Exception("There is no operator");
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                }
+            }
+
+
+            Console.WriteLine("= " + stack[0]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BDSA_2014/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDSA_2014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
