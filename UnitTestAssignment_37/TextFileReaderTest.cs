4509ce184b94870571d6a43ca7f889d0fec96d2d
Mehr <mehrdad.khodaverdi@outlook.com>
2014-09-26 12:45:39 +0200

    Unittest created

---
+++ UnitTestAssignment_37/TextFileReaderTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment_37;
+
+namespace UnitTestAssignment_37
+{
+    [TestClass]
+    public class TextFileReaderTest
+    {
+        [TestMethod]
+        public void DoSearching_Test()
+        {
+            // arrange
+
+
+            // act
+
+
+            // assert
+
+        }
+    }
+}
