bb9f6ffac05d1e32f118c3e769a9533719a60c01
Mehr <mehrdad.khodaverdi@outlook.com>
2014-09-26 14:32:49 +0200

    No more testing!!

--- UnitTestAssignment_37/TextFileReaderTest.cs
+++ UnitTestAssignment_37/TextFileReaderTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Specialized;
 using Assignment_37;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestAssignment_37
 {
@@ -11,12 +12,19 @@ namespace UnitTestAssignment_37
         public void DoSearching_Test()
         {
             // arrange
+            //string RegExUrl = @"(([\w-]+://?|www[.])[^\s()<>]+)\s+";
+            //string RegExDate = @"(Sun|Mon|Tue|Wed|Thu|Fri|Sat),\s+([0-9]?[1-9])\s+(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+[0-9]{4,4}\s+((2[0-3])|([0-1][0-9])):([0-5][0-9]):([0-5][0-9])\s+([-\+][0-9]{2}[0-5][0-9])";
+            //string content = TextFileReader.ReadFile("../../TestFile.txt");
+            //string[] keys = {"Isaac", "*"};
 
+            string regexExpected = @"\s(?i)Isaac\s[\w'-]*\s";
 
             // act
 
+            string testKeyQuery = TextFileReader.DoSearching();
 
             // assert
+            Assert.AreEqual(regexExpected, testKeyQuery,"Fail!");
 
         }
     }
